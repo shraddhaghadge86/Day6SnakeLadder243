677bfb09713c955e969dd835abbb1bec9d1ed9e7
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-11 20:23:52 +0530

    Added: Player rolls the die

--- com/bridgelabz/day6snakeladder/SnakeLadder.java
+++ com/bridgelabz/day6snakeladder/SnakeLadder.java
@@ -7,5 +7,9 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder ");
+
+            int die_Roll=(int)Math.floor((Math.random()*10)%7);
+            System.out.println(die_Roll);
+        }
     }
-}
+
