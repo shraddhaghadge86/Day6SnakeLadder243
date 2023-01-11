3eb334bb1c727cf7a44038fc2701f75e135bea51
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-11 20:32:37 +0530

    Added: Check for Position

--- com/bridgelabz/day6snakeladder/SnakeLadder.java
+++ com/bridgelabz/day6snakeladder/SnakeLadder.java
@@ -4,12 +4,30 @@ public class SnakeLadder {
 
     static public final int Starting_Position_Of_Player = 0;
     static public final int Ending_Position_Of_Player = 100;
-
+    public static final  int ladder = 2;
+    public static final int snake = 1;
+    public static final int noplay = 0;
+    public static int roolldie(){
+        int dievalue = (int)Math.floor(Math.random()*10)%6+1;
+        return dievalue ;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder ");
+        int roolldie=roolldie();
+        int currentPosition = 0;
+        int checkPosition= (int)Math.floor(Math.random()*10)%3;
 
-            int die_Roll=(int)Math.floor((Math.random()*10)%7);
-            System.out.println(die_Roll);
+        switch (checkPosition){
+            case snake:
+                currentPosition = roolldie-snake;
+                System.out.println("player is on snake " +checkPosition);
+                break;
+            case  ladder:
+                currentPosition = ladder+roolldie;
+                System.out.println("player is on Ladder " +currentPosition);
+                break;
+            default:
+                System.out.println("player is not play " );
         }
     }
+}
 
