2f1a8f5a62a92cdcd944d4a4b1b2b7297d8496aa
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-11 20:06:58 +0530

    Added: Snake and Ladder program

---
+++ com/bridgelabz/day6snakeladder/SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day6snakeladder;
+
+public class SnakeLadder {
+
+    static public final int Starting_Position_Of_Player = 0;
+    static public final int Ending_Position_Of_Player = 100;
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder ");
+    }
+}
